3602947bc7f94663d2bce8df271a1c95ee870f54
coder3101 <ashar786khan@gmail.com>
2018-04-01 03:48:15 +0530

    updated LICENSE on utils.cc

--- utils.cc
+++ utils.cc
@@ -1,3 +1,17 @@
+/*
+*   Copyright (c) Ashar Khan 2018. <ashar786khan@gmail.com>
+*    This file is part of CSE202 Car Workshop Project.
+*   Car Workshop is free software : you can redistribute it and/or modify
+*    it under the terms of GNU General Public License as published by the Free Software Foundation,
+*   either version 3 of the License, or (at your option) any later version.
+*
+*   This Application is distributed in the hope that it will be useful
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License along with this Source File.
+*   If not, see <http:www.gnu.org/licenses/>.
+*/
 
 
 #include <fstream>
